36aa01b8f37a1599f45c8894348197fe4c065aed
William Zhang <willzhang73@gmail.com>
2024-01-10 04:18:51 +0000

    prep-javascript-operators-and-expressions created

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,34 @@
+//console.log("hello");
+let number1=3;
+let number2=5;
+let product=number1*number2;
+console.log(`product: ${product}`);
+console.log(`typeof product: ${typeof product}`);
+let charge = 90.90;
+let payment = 60.0;
+let amountRemaining=charge - payment;
+console.log(`amountRemaining: ${amountRemaining}`);
+console.log(`type of amountRemaining: ${typeof amountRemaining}`);
+let tests=90.0;
+let assignments=88.8;
+let final = 96.0;
+let grade=(tests + assignments + final)/3
+console.log('grade: ', grade);
+console.log('type of grade: ', typeof grade);
+let firstName='William';
+let lastName='Zhang';
+let fullName=firstName+" "+lastName;
+console.log(`fullName: ${fullName}`);
+console.log(`type of fullName: ${fullName}`);
+let pH = 5.6
+let isAcidic= pH < 7;
+console.log('isAcidic: ', isAcidic);
+console.log('type of isAcidic: ', typeof isAcidic);
+let numberOfSoldiers=99;
+let isSparta=numberOfSoldiers===300;
+console.log('isSparta: ', isSparta);
+console.log('type of isSparta: ', typeof isSparta);
+let nickName=fullName+ ' is the GOAT';
+console.log('nickName: ', nickName);
+console.log('type of nickName: ', typeof nickName);
+
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,49 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What data type is returned by an arithmetic operation?
+Number 
+
+- What is string concatenation?
+concatenate two strings into one string value
+
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+concatenate two strings 
+sum up two numbers
+
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+Boolean 
+
+- What does the `+=` "plus-equals" operator do?
+plus one and then assign 
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
