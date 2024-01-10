3002dd58176c37afa093a0f5dc1c3402b7927d41
William Zhang <willzhang73@gmail.com>
2024-01-10 17:54:04 +0000

    prep-javascript-operators-and-expressions change let to const

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -1,34 +1,35 @@
 //console.log("hello");
-let number1=3;
-let number2=5;
-let product=number1*number2;
+const number1=3;
+const number2=5;
+const product=number1*number2;
 console.log(`product: ${product}`);
 console.log(`typeof product: ${typeof product}`);
-let charge = 90.90;
-let payment = 60.0;
-let amountRemaining=charge - payment;
+const charge = 90.90;
+const payment = 60.0;
+const amountRemaining=charge - payment;
 console.log(`amountRemaining: ${amountRemaining}`);
 console.log(`type of amountRemaining: ${typeof amountRemaining}`);
-let tests=90.0;
-let assignments=88.8;
-let final = 96.0;
-let grade=(tests + assignments + final)/3
+const tests=90.0;
+const assignments=88.8;
+const final = 96.0;
+const grade=(tests + assignments + final)/3
 console.log('grade: ', grade);
 console.log('type of grade: ', typeof grade);
-let firstName='William';
-let lastName='Zhang';
-let fullName=firstName+" "+lastName;
+const firstName='William';
+const lastName='Zhang';
+const fullName=firstName+" "+lastName;
 console.log(`fullName: ${fullName}`);
 console.log(`type of fullName: ${fullName}`);
-let pH = 5.6
-let isAcidic= pH < 7;
+const pH = 5.6
+const isAcidic= pH < 7;
 console.log('isAcidic: ', isAcidic);
 console.log('type of isAcidic: ', typeof isAcidic);
-let numberOfSoldiers=99;
-let isSparta=numberOfSoldiers===300;
+const numberOfSoldiers=99;
+const isSparta=numberOfSoldiers===300;
 console.log('isSparta: ', isSparta);
 console.log('type of isSparta: ', typeof isSparta);
-let nickName=fullName+ ' is the GOAT';
+let nickName=fullName;
+nickName=nickName+' is the GOAT';
 console.log('nickName: ', nickName);
 console.log('type of nickName: ', typeof nickName);
 
